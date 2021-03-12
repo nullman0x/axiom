2596f563957de9115299d5e83c492145eff58309
nullman0x <77861653+nullman0x@users.noreply.github.com>
2021-03-12 16:07:19 +0530

    Update linode.sh

--- interact/account-helpers/linode.sh
+++ interact/account-helpers/linode.sh
@@ -12,10 +12,6 @@ provider=""
 size=""
 email=""
 
-read acc 
-
-if [[ "$acc" == "n" ]]; then
-fi
 echo -e "${Green}Installing linode-cli\n ${Color_Off}"	
 sudo pip3 install linode-cli --upgrade
 #linode-cli 
@@ -39,11 +35,6 @@ read region
 	echo -e "${Blue}Selected default option 'g6-standard-1'${Color_Off}"
         size="g6-standard-1"
 fi
-
-
-read email
-
-read ans
 
 
 data="$(echo "{\"do_key\":\"$token\",\"region\":\"$region\",\"provider\":\"linode\",\"default_size\":\"$size\",\"appliance_name\":\"$appliance_name\",\"appliance_key\":\"$appliance_key\",\"appliance_url\":\"$appliance_url\", \"email\":\"$email\"}")"
@@ -70,4 +61,5 @@ fi
 echo $data | jq > "$AXIOM_PATH/accounts/$title.json"
 echo -e "${BGreen}Saved profile '$title' successfully!${Color_Off}"
 $AXIOM_PATH/interact/axiom-account $title
+
 
